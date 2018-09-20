9427aaa5e7462aafc024d3848f38a57eda91aa87
Qais Patankar <qaisjp@gmail.com>
2018-09-20 11:22:27 +0100

    lexer: add logical op check

--- src/lexer/Tokeniser.java
+++ src/lexer/Tokeniser.java
@@ -266,6 +266,29 @@ public class Tokeniser {
             }
         }
 
+        // Check for logical operators
+        if (c == '&' || c == '|') {
+            if (scanner.peek() == c) {
+                TokenClass tok = null;
+                switch (c) {
+                    case '&':
+                        tok = TokenClass.AND;
+                        break;
+                    case '|':
+                        tok = TokenClass.OR;
+                        break;
+                }
+
+                if (tok != null) {
+                    // Consume the character we've peeked
+                    scanner.next();
+
+                    // Return the token we've found
+                    return new Token(tok, line, column);
+                }
+            }
+        }
+
         // Use readstring trick to read strings (or individual characters)
         // starts with provided character
         TokenClass tok = readString(c);

e09e99d4e6e619dc21799e61869fd88db67a0747
Qais Patankar <qaisjp@gmail.com>
2018-10-17 03:12:20 +0100

    sem: add todo for uncommenting type checking

--- src/sem/SemanticAnalyzer.java
+++ src/sem/SemanticAnalyzer.java
@@ -8,6 +8,7 @@ public class SemanticAnalyzer {
 		// List of visitors
 		ArrayList<SemanticVisitor> visitors = new ArrayList<SemanticVisitor>() {{
 			add(new NameAnalysisVisitor());
+//			add(new TypeCheckVisitor()); todo uncomment line
 		}};
 		// Error accumulator
 		int errors = 0;

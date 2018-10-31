a46decca39770fa3a221390977079e703a043aa2
Qais Patankar <qaisjp@gmail.com>
2018-10-31 17:37:28 +0000

    gen: add build script

---
+++ tests/gen.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# First check if environment variable PROJ is set
+if [ -z "$PROJ" ]; then
+    echo "PROJ has not been set yet.";
+    exit 1;
+fi
+
+SHOULD_COMPILE=false;
+FILENAME="$1";
+if [ "$1" = "-c" ]; then
+    SHOULD_COMPILE=true;
+    FILENAME="$2";
+elif [ "$2" = "-c" ]; then
+    SHOULD_COMPILE=true;
+fi
+
+# Check if file exists
+if [ ! -f "$FILENAME" ]; then
+    echo "Filname \"$FILENAME\" does not exist";
+    exit 1;
+fi
+
+# Attempt build
+if $SHOULD_COMPILE; then
+    ant build -f "$PROJ/build.xml";
+fi
+
+# Cancel if can't build
+if [ "$?" -ne "0" ]; then
+    exit 1;
+fi
+
+echo "";
+echo "=== Generation output below ==="
+
+TARGET="$(mktemp)"
+
+java -ea -cp $PROJ/bin Main -gen "$FILENAME" "$TARGET"
+
+# Print out the target
+cat "$TARGET"
+
+# Delete the target
+rm "$TARGET"
+

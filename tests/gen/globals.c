b27e10d54ac921e71117523c14700c218009bf50
Qais Patankar <qaisjp@gmail.com>
2018-11-02 21:35:25 +0000

    tests/gen: add globals

---
+++ tests/gen/globals.c
@@ -0,0 +1,23 @@
+// Test arrays
+int intArray[5];
+char charArray[5];
+char* stringArray[2];
+
+// Test primitives
+int integer;
+char character;
+// void voids can't be tested;
+
+// Test pointers
+int* intPointer;
+char* charPointer;
+
+// Test 4-byte boundaries
+char charArray_be_1_01[1]; // should be 1
+char charArray_be_4_01[2]; // should be 4
+char charArray_be_4_02[3]; // should be 4
+char charArray_be_4_03[4]; // should be 4
+char charArray_be_8_01[5]; // should be 8
+char charArray_be_8_02[7]; // should be 8
+char charArray_be_8_03[8]; // should be 8
+char charArray_be_12_01[9]; // should be 12

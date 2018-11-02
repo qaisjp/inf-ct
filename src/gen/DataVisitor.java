f5efceec8c405f95fcd603b6a86eef303cb87cf5
Qais Patankar <qaisjp@gmail.com>
2018-11-02 16:55:18 +0000

    gen: handle some global vardecls

--- src/gen/DataVisitor.java
+++ src/gen/DataVisitor.java
@@ -5,6 +5,7 @@ import ast.*;
 public class DataVisitor extends TraverseVisitor<Void> {
     private IndentWriter writer;
     private Labeller strLabeller = new Labeller("str");
+    private Labeller globalLabeller = new Labeller("g");
 
     public DataVisitor(IndentWriter writer) {
         this.writer = writer;
@@ -14,7 +15,13 @@ public class DataVisitor extends TraverseVisitor<Void> {
     public Void visitProgram(Program p) {
         writer.printf(".data");
         writer.push();
-        super.visitProgram(p);
+
+        visitEach(p.structTypeDecls);
+        for (VarDecl v : p.varDecls) {
+            visitVarDeclGlobal(v);
+        }
+        visitEach(p.funDecls);
+
         writer.pop();
 
         assert writer.getLevel() == 0;
@@ -23,13 +30,28 @@ public class DataVisitor extends TraverseVisitor<Void> {
 
     @Override
     public Void visitStrLiteral(StrLiteral s) {
+        super.visitStrLiteral(s);
+
         s.genLabel = strLabeller.makeLabel();
         writer.printf("%s: .asciiz \"%s\"", s.genLabel, s.escapedString());
         return null;
     }
 
-    @Override
-    public Void visitVarDecl(VarDecl varDecl) {
+    public Void visitVarDeclGlobal(VarDecl varDecl) {
+        super.visitVarDecl(varDecl);
+
+        int size;
+        if (varDecl.varType instanceof ChrLiteral) {
+            size = 1;
+        } else if (varDecl.varType instanceof IntLiteral) {
+            size = 4;
+        } else if (varDecl.varType instanceof PointerType) {
+            size = 4;
+        } else {
+            throw new RuntimeException("Encountered unknown variable type in global variable declaration " + varDecl.varType.toString());
+        }
+
+        writer.printf("%s: .space %d", globalLabeller.makeLabel(varDecl.varName), size);
         return null;
     }
 }

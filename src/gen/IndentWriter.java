c3cdee2b7c6ce73a27892c4a95addcdfbf8d93de
Qais Patankar <qaisjp@gmail.com>
2018-11-03 03:02:48 +0000

    gen: add IndentWriter.withLabel

--- src/gen/IndentWriter.java
+++ src/gen/IndentWriter.java
@@ -6,6 +6,7 @@ public class IndentWriter {
     private PrintWriter writer;
     private int level = 0;
     private static final int width = 4;
+    private String label; // current label
 
     public IndentWriter(PrintWriter writer) {
         this.writer = writer;
@@ -19,10 +20,21 @@ public class IndentWriter {
         level -= 1;
     }
 
+    public IndentWriter withLabel(String label) {
+        this.label = label;
+        return this;
+    }
+
     public void printf(String format, Object... args) {
         String indentation = new String(new char[level*width]).replace("\0", " ");
 
+        if (!label.isEmpty()) {
+            label += ": ";
+        }
+
         writer.printf(indentation + format + "\n", args);
+
+        label = "";
     }
 
     public int getLevel() {

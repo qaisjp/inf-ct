d352d5a65be057284e0ac3e7dda67cc98abed508
Qais Patankar <qaisjp@gmail.com>
2018-11-03 03:35:27 +0000

    ast: impl VarDecl global labels

--- src/ast/VarDecl.java
+++ src/ast/VarDecl.java
@@ -3,6 +3,7 @@ package ast;
 public class VarDecl implements ASTNode {
     public final Type varType;
     public final String varName;
+    private String genLabel = null;
 
     public VarDecl(Type varType, String varName) {
 	    this.varType = varType;
@@ -16,5 +17,21 @@ public class VarDecl implements ASTNode {
     @Override
     public String toString() {
         return varType.toString() + " " + varName;
+    }
+
+
+    public void setGlobalLabel(String label) {
+        if (genLabel != null) {
+            throw new RuntimeException("Can't set label if already set");
+        }
+
+        genLabel = label;
+    }
+
+    public String getGlobalLabel() {
+        if (genLabel == null) {
+            throw new NullPointerException("Can't get global label if not a global label (genLabel is null)");
+        }
+        return genLabel;
     }
 }

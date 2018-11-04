0e66f097cba7e7e6b6c35b1d7d45e81341379977
Qais Patankar <qaisjp@gmail.com>
2018-11-04 04:24:09 +0000

    gen: writer .text in CodeGenerator

--- src/gen/CodeGenerator.java
+++ src/gen/CodeGenerator.java
@@ -20,16 +20,25 @@ public class CodeGenerator {
         IndentWriter indentWriter = new IndentWriter(writer);
         Registers registers = new Registers();
 
-        // List of visitors
+        // Visit the data visitor
+        program.accept(new DataVisitor(indentWriter));
+
+        // Prep for text
+        indentWriter.printf(".text");
+        indentWriter.push();
+
+        // List of text visitors
         ArrayList<ASTVisitor> visitors = new ArrayList<ASTVisitor>() {{
-            add(new DataVisitor(indentWriter));
             add(new InbuiltVisitor(indentWriter, registers));
         }};
 
-        // Apply each visitor to the AST
+        // Apply each text visitor to the AST
         for (ASTVisitor v : visitors) {
             program.accept(v);
         }
+
+        // Pop the text
+        indentWriter.pop();
 
         writer.close();
     }
--- src/gen/InbuiltVisitor.java
+++ src/gen/InbuiltVisitor.java
@@ -14,60 +14,6 @@ public class InbuiltVisitor extends TraverseVisitor<Register> {
         this.registers = registers;
     }
 
-    public List<Register> visitEach(List<? extends ASTNode> list) {
-        List<Register> results = new ArrayList<>();
-        for (ASTNode l : list) {
-            results.add(l.accept(this));
-        }
-
-        return results;
-    }
-
-    @Override
-    public Register visitBlock(Block b) {
-        visitEach(b.varDecls); // todo: scratch
-        visitEach(b.stmtList);// todo: scratch
-        return null;
-    }
-
-    @Override
-    public Register visitFunDecl(FunDecl p) {
-        // TODO: check if return null is ok
-        if (p.isInbuilt) { return null; }
-
-        // todo: scratch
-        writer.printf(p.name + ":\n");
-
-        // todo: scratch
-        visitEach(p.params);
-        p.block.accept(this);// todo: scratch
-
-        return null;
-    }
-
-    @Override
-    public Register visitProgram(Program p) {
-        // todo: empty program should generate empty asm file
-
-        writer.printf(".text\n"); // todo: scratch
-        visitEach(p.funDecls); // todo: scratch
-        return null;
-    }
-
-    @Override
-    public Register visitExprStmt(ExprStmt e) {
-        e.expr.accept(this); // todo: scratch
-        return null;
-    }
-
-    @Override
-    public Register visitIntLiteral(IntLiteral il) {
-        Register register = registers.get();
-        writer.printf("li\t%s, %d", register, il.value);
-        return register;
-    }
-
-
     @Override
     public Register visitFunCallExpr(FunCallExpr f) {
         if (f.decl.isInbuilt) {

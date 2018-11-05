897615382a97d084f0469524dfe0ed0460e7b43f
Qais Patankar <qaisjp@gmail.com>
2018-11-05 11:25:06 +0000

    gen: make InbuiltVisitor magical

--- src/gen/InbuiltVisitor.java
+++ src/gen/InbuiltVisitor.java
@@ -2,31 +2,48 @@ package gen;
 
 import ast.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.BiFunction;
 
 public class InbuiltVisitor extends TraverseVisitor<Register> {
-    private IndentWriter writer; // use this writer to output the assembly instructions
-    private Registers registers;
+    private static IndentWriter writer; // use this writer to output the assembly instructions
+    private static Registers registers;
+    private static HashMap<String, BiFunction<FunDecl, List<Expr>, Register>> inbuilts = null;
 
+    /*
+        1:  void print_i(int i);
+        4:  void print_s(char* s);
+        5:  int read_i();
+        9:  void* mcmalloc(int size);
+        11: void print_c(char c);
+        12: char read_c();
+    */
     public InbuiltVisitor() {
-        this.writer = V.writer;
-        this.registers = V.registers;
+        InbuiltVisitor.writer = V.writer;
+        InbuiltVisitor.registers = V.registers;
+
+        if (inbuilts == null) {
+            // Initialise inbuilts
+            InbuiltVisitor.inbuilts = new HashMap<>();
+            InbuiltVisitor.inbuilts.put("print_i", InbuiltVisitor::print_i);
+        }
     }
 
-    private Register print_i(FunDecl f, List<Expr> args) {
+    private static Register print_i(FunDecl f, List<Expr> args) {
         Expr arg = args.get(0);
 
         Register.v0.loadImmediate(1);
 
         if (arg instanceof IntLiteral) {
-            writer.li(Register.arg[0], ((IntLiteral) arg).value);
+            V.writer.li(Register.arg[0], ((IntLiteral) arg).value);
         } else {
             try (Register val = arg.accept(V.text)) {
                 Register.arg[0].set(val);
             }
         }
 
-        writer.syscall();
+        V.writer.syscall();
 
         return null;
     }
@@ -39,13 +56,11 @@ public class InbuiltVisitor extends TraverseVisitor<Register> {
 
         writer.leadNewline().comment("%s", f);
 
-        switch (f.decl.name) {
-            case "print_i":
-                return print_i(f.decl, f.exprList);
+        if (!inbuilts.containsKey(f.decl.name)) {
+            writer.comment("stub: %s", f); // todo: replace with RuntimeException
+            return null;
         }
 
-        writer.comment("stub: %s", f); // todo: replace with RuntimeException
-
-        return null; // todo fix this
+        return inbuilts.get(f.decl.name).apply(f.decl, f.exprList);
     }
 }

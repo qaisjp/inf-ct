84ae5e70d12830b383fe0acb36c67c144519920c
Qais Patankar <qaisjp@gmail.com>
2018-11-07 01:15:42 +0000

    ast: add getInnerType to ArrayAccessExpr

--- src/ast/ArrayAccessExpr.java
+++ src/ast/ArrayAccessExpr.java
@@ -4,9 +4,20 @@ public class ArrayAccessExpr extends Expr {
     public final Expr expr;
     public final Expr index;
 
+    private final String InnerTypeError = "Expression is not pointer or array";
+
     public ArrayAccessExpr(Expr expr, Expr index) {
         this.expr = expr;
         this.index = index;
+    }
+
+    public Type getInnerType() {
+        if (expr.type instanceof PointerType) {
+            return ((PointerType) expr.type).innerType;
+        } else if (expr.type instanceof ArrayType) {
+            return ((ArrayType) expr.type).elemType;
+        }
+        throw new RuntimeException(InnerTypeError + " is expr:" + expr.toString() + "type:" + expr.type.toString());
     }
 
     public <T> T accept(ASTVisitor<T> v) {

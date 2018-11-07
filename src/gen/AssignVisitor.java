ecac8b764487c41e79f6b85143f478436fb846da
Qais Patankar <qaisjp@gmail.com>
2018-11-07 01:24:20 +0000

    gen: refactor AssignVisitor to new storeValue

--- src/gen/AssignVisitor.java
+++ src/gen/AssignVisitor.java
@@ -9,24 +9,26 @@ public class AssignVisitor extends TraverseVisitor<Void> {
         this.writer = V.writer;
     }
 
-
+    // store
+    private void storeValue(Register sourceValue, Type type, Register targetAddress, int offset) {
+        if (type == BaseType.CHAR) {
+            sourceValue.storeByteAt(targetAddress, offset);
+        } else if (type == BaseType.INT || type instanceof PointerType) {
+            sourceValue.storeWordAt(targetAddress, offset);
+        } else {
+            // arrays and structs need SPECIAL treatment!
+            // oh and strings too topKEKKER
+            // todo
+            throw new RuntimeException(
+                    "STUB! arrays, structs (& maybe strings) haven't been implemented yet");
+        }
+    }
 
     private void assignVarExpr(VarExpr lhs, Register rReg) {
         try (Register lReg = V.text.getVarExprAddress(lhs)) {
             VarDecl decl = lhs.vd;
 
-            Type varType = decl.varType;
-            if (varType == BaseType.CHAR) {
-                rReg.storeByteAt(lReg, 0);
-            } else if (varType == BaseType.INT || varType instanceof PointerType) {
-                rReg.storeWordAt(lReg, 0);
-            } else {
-                // arrays and structs need SPECIAL treatment!
-                // oh and strings too topKEKKER
-                // todo
-                throw new RuntimeException(
-                        "STUB! arrays, structs (& maybe strings) haven't been implemented yet");
-            }
+            storeValue(rReg, decl.varType, lReg, 0);
         }
     }
 

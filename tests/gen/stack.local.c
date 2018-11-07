fe16ce89f366f744864c052828b17463fdd2eebb
Qais Patankar <qaisjp@gmail.com>
2018-11-07 03:30:26 +0000

    tests/gen: add stack.local.c

---
+++ tests/gen/stack.local.c
@@ -0,0 +1,43 @@
+/*gen:expect/*
+0
+1
+2
+Hi
+Hi
+/*gen:expect*/
+
+// todo: add struct test
+
+void main() {
+	int a;      // 4 bytes  4 (a word)
+	char b;     // 1 byte   4 (padded to 4)
+	char c[3];  // 3 bytes  4 (padded to 4)
+	char* d;    // 4 bytes  4 (a pointer)
+	            //
+	            // Total:   16 bytes
+
+	// Print default value of a
+	print_i(a);
+	print_c('\n');
+
+	// Print 1
+	a = 1;
+	print_i(a);
+	print_c('\n');
+
+	// Print a 2
+	b = '2';
+	print_c(b);
+	print_c('\n');
+
+	// Hi
+	c[0] = 'H';
+	c[1] = 'i';
+	c[2] = '\0';
+	print_s((char*) c);
+
+	print_c('\n');
+
+	d = (char*) c;
+	print_s(d);
+}

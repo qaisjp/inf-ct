6e3f85efa81628f2778c8f959e7f13ded079fdd0
Qais Patankar <qaisjp@gmail.com>
2019-01-10 11:31:54 +0000

    my: copy simple-dce to my-dce

--- part-4/passes/llvm-pass-my-dce/skeleton/Skeleton.cpp
+++ part-4/passes/llvm-pass-my-dce/skeleton/Skeleton.cpp
@@ -3,21 +3,86 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/Transforms/Utils/Local.h"
+#include <vector>
+
 using namespace llvm;
+
+/*
+SmallVector<Instruction*, 16> ul;
+
+while changed {
+  changed = false
+  for (instruction i) {
+    if i.isTriviallyDead() {
+      changed = true
+      ul.push(i)
+    }
+  }
+
+  while (!ul.empty()) {
+    node = ul.pop();
+    node.replaceAllwith(node.getCurrentValue(0)) // i think?
+    nodeblah
+}
+*/
 
 namespace {
   struct SkeletonPass : public FunctionPass {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
+    // searchAndDestroy returns true if something changed
+    bool searchAndDestroy(Function &F) {
+      SmallVector<Instruction*, 16> ul;
+
+      // Find dead instructions
+      for (Function::iterator bb = F.begin(); bb != F.end(); ++bb) {
+        for (BasicBlock::iterator i = bb->begin(); i != bb->end(); ++i) {
+          Instruction* inst = &*i;
+
+          if (isInstructionTriviallyDead(inst)) {
+            errs() << "instruction dead \n";
+            ul.push_back(inst);
+          } else {
+            errs() << "instruction alive: ";
+            inst->printAsOperand(errs());
+            errs() << "\n";
+
+            // errs() << "instruction alive " << inst->getOpcodeName() << "\n";
+          }
+        }
+      }
+
+      // Erase each instruction
+      for (Instruction* inst : ul) {
+        errs() << "instruction removed\n";
+        inst->eraseFromParent();
+      }
+
+      return !ul.empty();
+    }
+
     virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+      int pass = 0;
+      do {
+        pass += 1;
+        errs() << "\n# Function: " << F.getName() << " (pass " << pass << ")\n";
+      } while (searchAndDestroy(F));
+
+      errs() << "\n--------\n";
+
       return false;
     }
   };
 }
 
 char SkeletonPass::ID = 0;
+
+// Register the pass
+__attribute__((unused)) static RegisterPass<SkeletonPass> X(
+  "skeletonpass", "Simple dead code elimination"
+);
 
 // Automatically enable the pass.
 // http://adriansampson.net/blog/clangpass.html
